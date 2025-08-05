fd4e3d63d5500a576bec5156cb9cc8f8cba51225
web安全 <178897973@qq.com>
2025-08-05 20:12:45 +0800

    Update aaa.py

--- aaa.py
+++ aaa.py
@@ -4,4 +4,15 @@
 import base64
 print(1231231231212312312312123123)
 import os
+import urllib3
+
+# 创建连接池管理器
+http = urllib3.PoolManager()
+
+# 发送 GET 请求
+response = http.request('GET', 'http://www.baidu.com')
+
+# 解码并输出返回的 HTML 内容（前500字符预览）
+html_content = response.data.decode('utf-8', errors='ignore')
+print(html_content[:500])  # 只打印前500个字符以避免输出太多
 
